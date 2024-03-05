d795b2c5ae177bc37e3612c01e96ecabcdba0cfd
Harapanee <harasum111@gmail.com>
2024-03-05 15:53:14 +0900

    upgrade flutter pub

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_engineer_codecheck/presentation/router/app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  const scope = ProviderScope(child: MyApp());
-  runApp(scope);
+
+  //ウェブで動いているかどうか
+  if (kIsWeb) {
+    //デバイスプレビューを使う
+    const scope = ProviderScope(child: MyApp());
+    final devicePreview = DevicePreview(builder: (_) => scope);
+    runApp(devicePreview);
+  } else {
+    //そのまま動かす
+    const scope = ProviderScope(child: MyApp());
+    runApp(scope);
+  }
 }
